a07c72055736a06ccb399c0f090eba78378bd5c3
Andy Seo <andyseo1209@gmail.com>
2015-12-22 16:15:09 -0500

    deleted index, main, etc

--- main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import CurrentTemp from './components/CurrentTemp';
-ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<CurrentTemp />, document.getElementById('current'));
