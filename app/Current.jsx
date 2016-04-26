1d8657cb124f3d5964d603e6a75c51bfaaa8b11f
Andy Seo <andyseo1209@gmail.com>
2016-04-25 23:11:50 -0400

    updated Current

--- app/Current.jsx
+++ app/Current.jsx
@@ -4,30 +4,30 @@ import { Button } from 'react-bootstrap';
 var $ = require ('jquery');
 var Current = React.createClass ({
 
-  updateWeather:function(event){
-    console.log("button clicked");
-    var url = "https://api.forecast.io/forecast/94ccca721c20d8f61c8277044ecdc3d8/43.472,-80.533?units=si&exclude=minutely&callback=parseResponse";
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+  // updateWeather:function(event){
+  //   console.log("button clicked");
+  //   var url = "https://api.forecast.io/forecast/94ccca721c20d8f61c8277044ecdc3d8/43.472,-80.533?units=si&exclude=minutely&callback=parseResponse";
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.open("GET", url, true);
 
-    var parseResponse = function (data){
-       console.log(data.latitude);
-    }
+  //   var parseResponse = function (data){
+  //      console.log(data.latitude);
+  //   }
 
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          console.log(xhr.responseText);
-        } else {
-          console.error(xhr.statusText);
-        }
-      }
-    };
-    xhr.send();
-    console.log("sent");
-    // var json = JSON.parse(xhr);
-    //console.log(json);
-  },
+  //   xhr.onload = function (e) {
+  //     if (xhr.readyState === 4) {
+  //       if (xhr.status === 200) {
+  //         console.log(xhr.responseText);
+  //       } else {
+  //         console.error(xhr.statusText);
+  //       }
+  //     }
+  //   };
+  //   xhr.send();
+  //   console.log("sent");
+  //   // var json = JSON.parse(xhr);
+  //   //console.log(json);
+  // },
 
   componentDidMount:function() {
     this.serverRequest = $.get(this.props.source, function (result) {
